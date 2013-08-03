414b15405399969133f0db7d4f93269c76d9cd77
chatgris <jboyer@af83.com>
2013-08-03 15:39:32 +0200

    Update bob
    Signed-off-by: chatgris <jboyer@af83.com>

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,14 +1,26 @@
 defmodule Teenager do
+  import String, only: [ upcase: 1, ends_with?: 2]
+
+  def hey("") do
+    "Fine. Be that way!"
+  end
+
   def hey(sentence) do
     cond do
-      sentence == "" ->
-        "Fine. Be that way!"
-      sentence == String.upcase(sentence) ->
+      upcase?(sentence) ->
         "Woah, chill out!"
-      String.ends_with?(sentence, "?") ->
+      question?(sentence) ->
         "Sure."
       true ->
         "Whatever."
     end
+  end
+
+  defp upcase?(sentence) do
+    sentence == upcase(sentence)
+  end
+
+  defp question?(sentence) do
+    ends_with?(sentence, "?")
   end
 end
